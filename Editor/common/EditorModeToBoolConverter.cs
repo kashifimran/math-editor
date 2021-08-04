342da718b7dcf2faeaeda17b5ee47dd63d8cf555
Johann Dirry <movgp0@gmail.com>
2021-08-04 23:06:14 +0200

    cleaning up value converters

--- Editor/common/EditorModeToBoolConverter.cs
+++ Editor/common/EditorModeToBoolConverter.cs
@@ -4,24 +4,13 @@ using System.Windows.Data;
 
 namespace Editor
 {
+    [ValueConversion(typeof(EditorMode), typeof(bool))]
     public sealed class EditorModeToBoolConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            EditorMode mode = (EditorMode)value;
-            if (mode == EditorMode.Math)
-            {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
-        }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+            value is EditorMode mode && mode == EditorMode.Text;
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            value is bool mode && mode ? EditorMode.Text : EditorMode.Math;
     }
 }
--- Editor/common/EditorModeToVisibilityConverter.cs
+++ Editor/common/EditorModeToVisibilityConverter.cs
@@ -5,24 +5,13 @@ using System.Windows.Data;
 
 namespace Editor
 {
+    [ValueConversion(typeof(EditorMode), typeof(Visibility))]
     public sealed class EditorModeToVisibilityConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            EditorMode mode = (EditorMode)value;
-            if (mode == EditorMode.Math)
-            {
-                return Visibility.Collapsed;
-            }
-            else
-            {
-                return Visibility.Visible;
-            }
-        }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+            value is EditorMode mode && mode == EditorMode.Math ? Visibility.Collapsed : Visibility.Visible;
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            value is Visibility mode && mode == Visibility.Collapsed ? EditorMode.Math : EditorMode.Text;
     }
 }
